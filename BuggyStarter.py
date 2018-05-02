d323727f94f0ee1ba64a32f713bccaec4b93198a
Sarah Townson <sarah.townson@scienceoxford.com>
2018-05-02 13:06:20 +0100

    Update BuggyStarter.py

--- BuggyStarter.py
+++ BuggyStarter.py
@@ -6,26 +6,26 @@ off = 1023
 
 
 def stop(time):
-    pin14.write_digital(off)
-    pin13.write_digital(off)
-    pin12.write_digital(off)
-    pin15.write_digital(off)
+    pin14.write_analog(off)
+    pin13.write_analog(off)
+    pin12.write_analog(off)
+    pin15.write_analog(off)
     sleep(time)
 
 
 def forward(time):
-    pin14.write_digital(on)
-    pin13.write_digital(off)
-    pin12.write_digital(on)
-    pin15.write_digital(off)
+    pin14.write_analog(on)
+    pin13.write_analog(off)
+    pin12.write_analog(on)
+    pin15.write_analog(off)
     sleep(time)
 
 
 def backward(time):
-    pin14.write_digital(off)
-    pin13.write_digital(on)
-    pin12.write_digital(off)
-    pin15.write_digital(on)
+    pin14.write_analog(off)
+    pin13.write_analog(on)
+    pin12.write_analog(off)
+    pin15.write_analog(on)
     sleep(time)
 
 
